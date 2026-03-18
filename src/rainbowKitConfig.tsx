fa97fef10d14bd210d790e06f414dc4668eae4e5
Yjasik <motoligin1@gmail.com>
2026-03-18 12:08:39 +0300

    Initial clean commit

---
+++ src/rainbowKitConfig.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import { getDefaultConfig } from "@rainbow-me/rainbowkit";
+import { sepolia } from "wagmi/chains";
+
+export default getDefaultConfig({
+  appName: "Crowdfunding",
+  projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
+  chains: [sepolia],
+  ssr: true,
+});
